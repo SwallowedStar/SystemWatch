d067c3a6a89807fbcac4d5e254e4d7eaa1fe3909
devinou971 <damiendasseux@gmail.com>
2022-12-14 13:29:34 +0100

    added coreUsage to scrap

--- scrap/scrap.py
+++ scrap/scrap.py
@@ -57,31 +57,30 @@ def calc_generale(incr):
             min_cpu=freq_cpu.min
             max_cpu=freq_cpu.max
             import clr as clllr
-            file = "OpenHardwareMonitorLib"
 
+            file = "OpenHardwareMonitorLib"
             clllr.AddReference(file)
             from OpenHardwareMonitor import Hardware
             handle = Hardware.Computer()
             handle.CPUEnabled = True
             handle.Open()
-            core_frequence=0
+            core_frequences=0
             compt=0
             for i in handle.Hardware:
                 i.Update()
                 for sensor in i.Sensors:
                     if sensor.SensorType.ToString() == "Clock" and "Core" in sensor.Name:
-                        core_frequence += sensor.Value
+                        core_frequences += sensor.Value
                         compt+=1
-            core_frequence=core_frequence/compt
+            core_frequences=core_frequences/compt
             if (gpu == []):
                 # rajouter min max freq
-                return [ram_usage, 0, 0, core_frequence,min_cpu,max_cpu, 0, 0]
+                return [ram_usage, 0, 0, ps.cpu_freq(),min_cpu,max_cpu, 0, 0]
             else:
                 gpu = GPUtil.getGPUs()[0]
                 gpu_temp = gpu.temperature
                 gpu_usage = gpu.load
                 vram_usage = gpu.memoryUsed
-
                 return [ram_usage, gpu_temp, gpu_usage, freq_cpu,min_cpu,max_cpu, vram_usage, 0]
         elif(os_name=="Linux"):
             freq_cpu = ps.cpu_freq()
@@ -89,9 +88,7 @@ def calc_generale(incr):
             min_cpu=freq_cpu.min
             max_cpu=freq_cpu.max
             if (gpu == []):
-
                 # rajouter min max freq
-
                 return [ram_usage, 0, 0, freq_cpu_current,min_cpu,max_cpu, 0, 0]
             else:
                 gpu = GPUtil.getGPUs()[0]
@@ -99,21 +96,18 @@ def calc_generale(incr):
                 gpu_usage = gpu.load
                 # Only on Linux
                 vram_usage = gpu.memoryUsed
-
-
                 return [ram_usage, gpu_temp, gpu_usage, freq_cpu_current,min_cpu,max_cpu, vram_usage, 0]
 
     elif (incr == 3):
         if(os_name=="Windows"):
             import clr as clllr
             file = "OpenHardwareMonitorLib"
-            
             clllr.AddReference(file)
             from OpenHardwareMonitor import Hardware
             handle = Hardware.Computer()
             handle.CPUEnabled = True
             handle.Open()
-            core_frequence=[]
+            core_frequences=[]
             core_temperatures=[]
             core_usages = []
             # TODO: test the core_usage 
@@ -121,29 +115,29 @@ def calc_generale(incr):
                 i.Update()
                 for sensor in i.Sensors:
                     if (sensor.SensorType.ToString() == "Clock" and "Core" in sensor.Name):
-                        core_frequence.append(sensor.Value)
+                        core_frequences.append(sensor.Value)
                     elif(sensor.SensorType.ToString()=="Temperature" and "Core" in sensor.Name):
                         core_temperatures.append(sensor.Value)
                     elif(sensor.Name == "CPU Core"):
                         core_usages.append(sensor.Value)
-            return [core_frequence,core_temperatures]
+            return [core_frequences,core_temperatures]
 
         elif(os_name=="Linux"):
             all_temp_cores = ps.sensors_temperatures()
             all_freq = ps.cpu_freq(percpu=True)
             all_percentages = ps.cpu_percent(1, percpu=True)
             core_temperatures = []
-            core_frequence = []
+            core_frequences = []
             core_usages = []
             for element in all_temp_cores["coretemp"]:
                 if ("Core" in element.label):
                     core_temperatures.append(element.current)
             for element in all_freq:
-                core_frequence.append(element.current)
+                core_frequences.append(element.current)
             for element in all_percentages:
                 core_usages.append(element)
 
-            return [core_temperatures, core_frequence, core_usages]
+            return [core_temperatures, core_frequences, core_usages]
 
 
 def res_func(res):
@@ -154,9 +148,10 @@ def res_func(res):
 
 if __name__ == "__main__":
     boolean=True
+    pc_exist = False
+    computer = None
 
     while boolean:
-        #print("Lancement processus")
         load_dotenv()
         IP_HOST= os.getenv('IP_HOST')
         LISTEN_PORT=os.getenv("LISTEN_PORT")
@@ -166,6 +161,8 @@ if __name__ == "__main__":
         nb_proc = 4
         incr = 0
         res = []
+
+
         with ctx.Pool(processes=nb_proc) as p:
             date_deb = datetime.datetime.now()
             for x in range(0, nb_proc):
@@ -173,9 +170,6 @@ if __name__ == "__main__":
                 incr += 1
             p.close()
             p.join()
-
-           
-            #print(datetime.datetime.now() - date_deb)
             p.terminate()
 
             #Now lets do the request
@@ -186,75 +180,59 @@ if __name__ == "__main__":
             l_general_infos=[]
             for x in res_l:
                 if(len(x)==2):
+                    l_thread_proc=[x]
+                if(len(x)==3):
                     if(isinstance(x[0], list)):
                         l_multiple_core=[x]
-                    else:
-                        l_thread_proc=[x]
+                        
                 elif(len(x)==7):
                     l_general_infos=[x]
 
                 elif(len(x)==8):
                     l_various_usage=[x]
-
-         
-
-            r=rq.get("http://"+IP_HOST+":"+LISTEN_PORT+"/api/computer/find/"+l_general_infos[0][0])
-            data=r.json()
-            pc_exist=True
             time_send=str(datetime.datetime.now())
-            if("error" in data):
-                pc_exist=False
-                
-                json_cpu={
-                    "CPUname":l_general_infos[0][1],
-                    "coreNumber":l_general_infos[0][4],
-                    "minFrequency":l_various_usage[0][4],
-                    "maxFrequency":l_various_usage[0][5]
-                }
-                json_info_computer={
-                    "computerName":l_general_infos[0][0],
-                    "GPUname":l_general_infos[0][2],
-                    "amountRAM":l_general_infos[0][5],
-                    "amountVRAM":l_general_infos[0][6],
-                    "CPU":json_cpu,
-                    "osName":l_general_infos[0][3]
-                }
-                json_monitor={
-                    "time":time_send,
-                    "RAMusage":l_various_usage[0][0],
-                    "nbThreads":l_thread_proc[0][0],
-                    "nbProcesses":l_thread_proc[0][1],
-                    "GPUtemp":l_various_usage[0][1],
-                    "CPUfreq":l_various_usage[0][3],
-                    "VRAMusage":l_various_usage[0][6],
-                    "electricalConsumption":0
-                }
-                rq.post("http://"+IP_HOST+":"+LISTEN_PORT+"/api/computer/complete",json=json_info_computer)
-                rq.post("http://"+IP_HOST+":"+LISTEN_PORT+"/api/monitor",json=json_monitor)
-
-
-            elif(pc_exist==True):
-                json_monitor={
-                    "time":time_send,
-                    "computerID":data["computerID"],
-                    "RAMusage":l_various_usage[0][0],
-                    "nbThreads":l_thread_proc[0][0],
-                    "nbProcesses":l_thread_proc[0][1],
-                    "GPUtemp":l_various_usage[0][1],
-                    "CPUfreq":l_various_usage[0][3],
-                    "VRAMusage":l_various_usage[0][6],
-                    "electricalConsumption":0
-                }
             
+            if not pc_exist:
+                r=rq.get("http://"+IP_HOST+":"+LISTEN_PORT+"/api/computer/find/"+l_general_infos[0][0])
+                computer=r.json()
+                if "error" in computer:
+                    json_cpu={
+                        "CPUname":l_general_infos[0][1],
+                        "coreNumber":l_general_infos[0][4],
+                        "minFrequency":l_various_usage[0][4],
+                        "maxFrequency":l_various_usage[0][5]
+                    }
+                    json_info_computer={
+                        "computerName":l_general_infos[0][0],
+                        "GPUname":l_general_infos[0][2],
+                        "amountRAM":l_general_infos[0][5],
+                        "amountVRAM":l_general_infos[0][6],
+                        "CPU":json_cpu,
+                        "osName":l_general_infos[0][3]
+                    }
+                    computer = rq.post("http://"+IP_HOST+":"+LISTEN_PORT+"/api/computer/complete",json=json_info_computer).json()
+                else : 
+                    pc_exist = True
+            else :
+                json_monitor={
+                    "time":time_send,
+                    "computerID":computer["computerID"],
+                    "RAMusage":l_various_usage[0][0],
+                    "nbThreads":l_thread_proc[0][0],
+                    "nbProcesses":l_thread_proc[0][1],
+                    "GPUtemp":l_various_usage[0][1],
+                    "CPUfreq":l_various_usage[0][3],
+                    "VRAMusage":l_various_usage[0][6],
+                    "electricalConsumption":0
+                }
 
                 rq.post("http://"+IP_HOST+":"+LISTEN_PORT+"/api/monitor",json=json_monitor)
-        
 
                 i=0
-                for x in data["cores"]:
+                for x in computer["cores"]:
                     json_core={
                         "time":time_send,                    
-                        "computerID":data["computerID"],
+                        "computerID":computer["computerID"],
                         "idCore":x["idCore"],
                         "coreTemp":l_multiple_core[0][0][i],
                         "coreFrequency":l_multiple_core[0][1][i],
@@ -263,5 +241,4 @@ if __name__ == "__main__":
                     i+=1
                     rq.post("http://"+IP_HOST+":"+LISTEN_PORT+"/api/corestatus",json=json_core)
 
-                
             time.sleep(0.5)
