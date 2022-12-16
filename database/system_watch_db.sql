a10c9475816910287b0c7fceb3d9b74e44a18269
devinou971 <damiendasseux@gmail.com>
2022-12-16 16:39:04 +0100

    updated database creation script

--- database/system_watch_db.sql
+++ database/system_watch_db.sql
@@ -1,176 +1,173 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.31, for Linux (x86_64)
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : mer. 16 nov. 2022 à 23:14
--- Version du serveur : 5.7.36
--- Version de PHP : 7.4.26
-
-SET FOREIGN_KEY_CHECKS=0;
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: system_watch_db
+-- ------------------------------------------------------
+-- Server version	8.0.31
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données : `system_watch_db`
---
-CREATE DATABASE IF NOT EXISTS `system_watch_db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `system_watch_db`;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `computer`
+-- Table structure for table `computer`
 --
 
 DROP TABLE IF EXISTS `computer`;
-CREATE TABLE IF NOT EXISTS `computer` (
-  `computerID` int(11) NOT NULL AUTO_INCREMENT,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `computer` (
+  `computerID` int NOT NULL AUTO_INCREMENT,
   `computerName` varchar(100) NOT NULL,
   `GPUname` varchar(100) NOT NULL,
-  `amountRAM` bigint(11) UNSIGNED NOT NULL,
-  `amountVRAM` bigint(11) UNSIGNED NOT NULL,
-  `CPUid` int(11) NOT NULL,
-  `osName` varchar(50) NULL,
+  `amountRAM` bigint unsigned NOT NULL,
+  `amountVRAM` bigint unsigned NOT NULL,
+  `CPUid` int NOT NULL,
+  `osName` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`computerID`),
-  KEY `fk_computer_cpu_CPUid` (`CPUid`)
-) ENGINE=InnoDB AUTO_INCREMENT=193 DEFAULT CHARSET=latin1;
+  KEY `fk_computer_cpu_CPUid` (`CPUid`),
+  CONSTRAINT `fk_computer_cpu_CPUid` FOREIGN KEY (`CPUid`) REFERENCES `cpu` (`CPUid`)
+) ENGINE=InnoDB AUTO_INCREMENT=218 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `computer`
+-- Dumping data for table `computer`
 --
 
-INSERT INTO `computer` (`computerID`, `computerName`, `GPUname`, `amountRAM`, `amountVRAM`, `CPUid`) VALUES
-(1, 'Hp envy', 'AMD Radeon', 12000000000, 6000000000, 1);
-
--- --------------------------------------------------------
+LOCK TABLES `computer` WRITE;
+/*!40000 ALTER TABLE `computer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `computer` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `core`
+-- Table structure for table `core`
 --
 
 DROP TABLE IF EXISTS `core`;
-CREATE TABLE IF NOT EXISTS `core` (
-  `idCore` int(11) NOT NULL AUTO_INCREMENT,
-  `computerID` int(11) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `core` (
+  `idCore` int NOT NULL AUTO_INCREMENT,
+  `computerID` int NOT NULL,
   PRIMARY KEY (`idCore`),
-  KEY `fk_core_computer_computerID` (`computerID`)
-) ENGINE=InnoDB AUTO_INCREMENT=839 DEFAULT CHARSET=latin1;
+  KEY `fk_core_computer_computerID` (`computerID`),
+  CONSTRAINT `fk_core_computer_computerID` FOREIGN KEY (`computerID`) REFERENCES `computer` (`computerID`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=19360 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `core`
+-- Dumping data for table `core`
 --
 
-INSERT INTO `core` (`idCore`, `computerID`) VALUES
-(1, 1),
-(2, 1),
-(3, 1),
-(4, 1),
-(5, 1),
-(6, 1);
-
--- --------------------------------------------------------
+LOCK TABLES `core` WRITE;
+/*!40000 ALTER TABLE `core` DISABLE KEYS */;
+/*!40000 ALTER TABLE `core` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `corestatus`
+-- Table structure for table `corestatus`
 --
 
 DROP TABLE IF EXISTS `corestatus`;
-CREATE TABLE IF NOT EXISTS `corestatus` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `corestatus` (
   `time` datetime NOT NULL,
-  `computerID` int(11) NOT NULL,
-  `idCore` int(11) NOT NULL,
+  `computerID` int NOT NULL,
+  `idCore` int NOT NULL,
   `coreFrequency` float NOT NULL,
   `coreTemp` float NOT NULL,
+  `coreUsage` double DEFAULT '0',
   PRIMARY KEY (`time`,`idCore`,`computerID`),
   KEY `fk_corestatus_core` (`idCore`),
-  KEY `fk_corestatus_monitor_computerID` (`computerID`)
+  KEY `fk_corestatus_monitor_computerID` (`computerID`),
+  CONSTRAINT `fk_corestatus_core` FOREIGN KEY (`idCore`) REFERENCES `core` (`idCore`) ON DELETE CASCADE,
+  CONSTRAINT `fk_corestatus_monitor_computerID` FOREIGN KEY (`computerID`) REFERENCES `monitor` (`computerID`) ON DELETE CASCADE,
+  CONSTRAINT `fk_corestatus_monitor_time` FOREIGN KEY (`time`) REFERENCES `monitor` (`time`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `cpu`
+-- Dumping data for table `corestatus`
+--
+
+LOCK TABLES `corestatus` WRITE;
+/*!40000 ALTER TABLE `corestatus` DISABLE KEYS */;
+/*!40000 ALTER TABLE `corestatus` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cpu`
 --
 
 DROP TABLE IF EXISTS `cpu`;
-CREATE TABLE IF NOT EXISTS `cpu` (
-  `CPUid` int(11) NOT NULL AUTO_INCREMENT,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cpu` (
+  `CPUid` int NOT NULL AUTO_INCREMENT,
   `CPUname` varchar(100) NOT NULL,
-  `coreNumber` int(11) NOT NULL,
-  `minFrequency` double UNSIGNED NOT NULL,
-  `maxFrequency` double UNSIGNED NOT NULL,
+  `coreNumber` int NOT NULL,
+  `minFrequency` double unsigned NOT NULL,
+  `maxFrequency` double unsigned NOT NULL,
   PRIMARY KEY (`CPUid`)
-) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `cpu`
+-- Dumping data for table `cpu`
 --
 
-INSERT INTO `cpu` (`CPUid`, `CPUname`, `coreNumber`, `minFrequency`, `maxFrequency`) VALUES
-(1, 'AMD Ryzen 5 4500U', 6, 0, 2380),
-(10, 'Intel i5-3 @ 3.800GHz', 4, 0, 3400000000);
-
--- --------------------------------------------------------
+LOCK TABLES `cpu` WRITE;
+/*!40000 ALTER TABLE `cpu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cpu` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `monitor`
+-- Table structure for table `monitor`
 --
 
 DROP TABLE IF EXISTS `monitor`;
-CREATE TABLE IF NOT EXISTS `monitor` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `monitor` (
   `time` datetime NOT NULL,
-  `computerID` int(11) NOT NULL,
-  `RAMusage` double UNSIGNED NOT NULL,
-  `nbThreads` int(11) NOT NULL,
-  `nbProcesses` int(11) NOT NULL,
+  `computerID` int NOT NULL,
+  `RAMusage` double unsigned NOT NULL,
+  `nbThreads` int NOT NULL,
+  `nbProcesses` int NOT NULL,
   `GPUtemp` double NOT NULL,
-  `CPUfreq` double UNSIGNED NOT NULL,
-  `VRAMusage` double UNSIGNED NOT NULL,
+  `CPUfreq` double unsigned NOT NULL,
+  `VRAMusage` double unsigned NOT NULL,
   `electricalConsumption` double NOT NULL,
   PRIMARY KEY (`time`,`computerID`),
-  KEY `fk_monitor_computer_computerid` (`computerID`)
+  KEY `fk_monitor_computer_computerid` (`computerID`),
+  CONSTRAINT `fk_monitor_computer_computerid` FOREIGN KEY (`computerID`) REFERENCES `computer` (`computerID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Contraintes pour les tables déchargées
+-- Dumping data for table `monitor`
 --
 
---
--- Contraintes pour la table `computer`
---
-ALTER TABLE `computer`
-  ADD CONSTRAINT `fk_computer_cpu_CPUid` FOREIGN KEY (`CPUid`) REFERENCES `cpu` (`CPUid`);
+LOCK TABLES `monitor` WRITE;
+/*!40000 ALTER TABLE `monitor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `monitor` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Contraintes pour la table `core`
---
-ALTER TABLE `core`
-  ADD CONSTRAINT `fk_core_computer_computerID` FOREIGN KEY (`computerID`) REFERENCES `computer` (`computerID`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `corestatus`
---
-ALTER TABLE `corestatus`
-  ADD CONSTRAINT `fk_corestatus_core` FOREIGN KEY (`idCore`) REFERENCES `core` (`idCore`) ON DELETE CASCADE,
-  ADD CONSTRAINT `fk_corestatus_monitor_computerID` FOREIGN KEY (`computerID`) REFERENCES `monitor` (`computerID`) ON DELETE CASCADE,
-  ADD CONSTRAINT `fk_corestatus_monitor_time` FOREIGN KEY (`time`) REFERENCES `monitor` (`time`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `monitor`
---
-ALTER TABLE `monitor`
-  ADD CONSTRAINT `fk_monitor_computer_computerid` FOREIGN KEY (`computerID`) REFERENCES `computer` (`computerID`) ON DELETE CASCADE;
-SET FOREIGN_KEY_CHECKS=1;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-16 15:37:28
+
